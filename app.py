b5c881e4d608ccff77d0aaaf5943eb04e58d08a2
VasudevGupta7 <7vasudevgupta@gmail.com>
2020-12-25 21:47:38 +0530

    using bart-xsum

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from pipeline import Pipeline
 
-from transformers import MBartForConditionalGeneration, MBartTokenizer
+from transformers import MBartForConditionalGeneration, MBartTokenizer, BartForConditionalGeneration, BartTokenizer
 
 @st.cache(allow_output_mutation=True)
 def get_model(translator_id, summarizer_id):
@@ -10,14 +10,14 @@ def get_model(translator_id, summarizer_id):
     t_model = MBartForConditionalGeneration.from_pretrained(translator_id)
     t_tokenizer = MBartTokenizer.from_pretrained(translator_id)
 
-    s_model = MBartForConditionalGeneration.from_pretrained(summarizer_id)
-    s_tokenizer = MBartTokenizer.from_pretrained(summarizer_id)
+    s_model = BartForConditionalGeneration.from_pretrained(summarizer_id)
+    s_tokenizer = BartTokenizer.from_pretrained(summarizer_id)
 
     return {"model": t_model, "tokenizer": t_tokenizer}, {"model": s_model, "tokenizer": s_tokenizer}
 
 if __name__ == '__main__':
 
-    translator, summarizer = get_model(translator_id="vasudevgupta/mbart-iitb-hin-eng", summarizer_id="facebook/mbart-large-cc25")
+    translator, summarizer = get_model(translator_id="vasudevgupta/mbart-iitb-hin-eng", summarizer_id="facebook/bart-large-xsum")
 
     st.write("""
     # Cool NLP demo's :)
@@ -25,17 +25,18 @@ if __name__ == '__main__':
     """)
 
     paragraph = st.text_area("Write a paragraph in hindi")
-    min_length = st.slider("Min. length of paragraph", min_value=8, max_value=16)
-    max_length = st.slider("Max. length of paragraph", min_value=16, max_value=256)
+    button = st.button("update")
+    min_length = st.slider("Min. length of paragraph", min_value=2, max_value=16)
+    max_length = st.slider("Max. length of paragraph", min_value=32, max_value=256)
 
     print({
         "paragraph": paragraph,
-        "num_tokens": len(paragraph.split(" ")),
+        "num_tokens": len(paragraph.split()),
         "min_length": min_length,
         "max_length": max_length
     })
 
-    if paragraph:
+    if paragraph and button:
         print("initiating pipeline")
         pl = Pipeline(translator, summarizer)
         summary, translation = pl(paragraph, min_length=min_length, max_length=max_length)
